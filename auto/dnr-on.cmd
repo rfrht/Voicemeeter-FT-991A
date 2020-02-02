6d515c445c47fdaa061d8d6867caa52616e5eb95
Rodrigo Freire <py2raf@rf3.org>
2020-02-02 18:36:22 -0300

    Scripts: AGC settings when using DNR
    * AGC fast when using DNR on; revert to AGC slow when
      turning DNR off;
    * New scripts for PTT on/off

--- auto/dnr-on.cmd
+++ auto/dnr-on.cmd
@@ -1,4 +1,4 @@
 @echo off
 mode COM4 BAUD=38400 PARITY=n DATA=8> NUL
-SET CAT=NR01;
+SET CAT=NR01;GT01;
 echo| set /p="%CAT%"> COM4
--- auto/ptt-OFF.cmd
+++ auto/ptt-OFF.cmd
@@ -1,4 +1,4 @@
 @echo off
 mode COM4 BAUD=38400 PARITY=n DATA=8 > NUL
-SET CAT=NR00;GT03;
+SET CAT=TX0;
 echo| set /p="%CAT%"> COM4
