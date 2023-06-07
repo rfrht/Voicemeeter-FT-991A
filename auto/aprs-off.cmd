addbe7e6b7fa891416d918c5e2b5ad703e5eee1a
Rodrigo Freire <py2raf@rf3.org>
2023-06-07 19:14:49 -0300

    Macros: Few updates
    APRS: Now uses Vox mode (why: https://github.com/rfrht/FT-991A/wiki/FT-991A-as-an-APRS-Digipeater)
    APRS: Updated buttons in Digital button set
    SSB: DNR pulls in AGC Fast

--- auto/aprs-off.cmd
+++ auto/aprs-off.cmd
@@ -1,9 +1,9 @@
 @echo off
 call %USERPROFILE%\auto\setup.cmd
 
-REM Squelch 30, Menu 076 DAKY
+REM Squelch 30, Menu 076 DAKY, Vox Off
 
-set CAT=EX0760;
+set CAT=EX0760;VX0;
 
 echo| set /p="%CAT%"> %COMPORT%
 
--- auto/aprs-on.cmd
+++ auto/aprs-on.cmd
@@ -1,9 +1,9 @@
 @echo off
 call %USERPROFILE%\auto\setup.cmd
 
-Recall Memory 020, menu 076 RTS, Vox 30 ms
+Recall Memory 020, menu 076 RTS, Vox 30 ms, vox on
 
-set CAT=MC020;EX0761;EX1470030;
+set CAT=MC020;EX0761;EX1470030;VX1;
 
 echo| set /p="%CAT%"> %COMPORT%
 
